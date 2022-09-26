3da7a7d2e24d46ae2a13c7817b853d6fa7696244
hustle-ps <grace.devolop@gmail.com>
2022-09-26 20:19:34 +0200

    done with navbar both mobile and desktop

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Nav.css';
+import TalentIcon from '../../asset/logo.png'
 
 function NavBar() {
   return (
-    <div className='nav'>
-      Nav
-    </div>
+    <header>
+      <img src={TalentIcon} className="logo" alt='log' />
+      <nav className='nav'>
+        <button className='btn-secondary'>ENG</button>
+        <button className='btn-secondary active'>FR</button>
+      </nav>
+    </header>
+    
   )
 }
 
